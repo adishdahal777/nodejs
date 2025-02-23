28f182c1e19b5471a01b7a75d311dcbf55641641
Adi <me.adishdahal@gmail.com>
2025-02-23 20:01:31 +0545

    iii

--- nodejs/controller/UserController.js
+++ nodejs/controller/UserController.js
@@ -21,5 +21,31 @@ const createUser = async (req, res) => {
     }
 };
 
+const getUser = async (req, res) => {
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (error) {
+        console.error("Error getting users:", error);
+        res.status(500).json({ message: "Error getting users", error: error.message });
+    }
+};
 
-export { createUser };
+const deleteUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const user = await User.findById(id);
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        await user.deleteOne();
+        res.status(200).json({ message: "User deleted" });
+    } catch (error) {
+        console.error("Error deleting user:", error);
+        res.status(500).json({ message: "Error deleting user", error: error.message });
+    }
+};
+
+export { createUser, getUser, deleteUser };
--- nodejs/routes/web.js
+++ nodejs/routes/web.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { createUser } from "../controller/UserController.js";
+import { createUser, getUser, deleteUser } from "../controller/UserController.js";
 const router = Router();
 
 
 router.get("/", (req, res) => {
     res.send("Hello World");
 });
+router.get("/user/list", getUser); 
 router.post("/user", createUser); 
+router.delete("/user/:id/delete", deleteUser);
 
 export  {router};
